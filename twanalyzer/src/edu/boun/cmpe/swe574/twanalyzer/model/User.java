17ff97d82a8f02ede0d0f5cbebfc8b4ebbfe6c06
mustafakosker <mustafakosker@gmail.com>
2013-11-21 17:52:54 +0200

    DB Model Classes.

---
+++ twanalyzer/src/edu/boun/cmpe/swe574/twanalyzer/model/User.java
@@ -0,0 +1,98 @@
+package edu.boun.cmpe.swe574.twanalyzer.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the users database table.
+ * 
+ */
+@Entity
+@Table(name="users")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="user_id")
+	private String userId;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="created_at")
+	private Date createdAt;
+
+	private String email;
+
+	@Column(name="email_confirmed")
+	private int emailConfirmed;
+
+	@Column(name="email_token")
+	private String emailToken;
+
+	@Column(name="full_name")
+	private String fullName;
+
+	private String password;
+
+	public User() {
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Date getCreatedAt() {
+		return this.createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getEmailConfirmed() {
+		return this.emailConfirmed;
+	}
+
+	public void setEmailConfirmed(int emailConfirmed) {
+		this.emailConfirmed = emailConfirmed;
+	}
+
+	public String getEmailToken() {
+		return this.emailToken;
+	}
+
+	public void setEmailToken(String emailToken) {
+		this.emailToken = emailToken;
+	}
+
+	public String getFullName() {
+		return this.fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
