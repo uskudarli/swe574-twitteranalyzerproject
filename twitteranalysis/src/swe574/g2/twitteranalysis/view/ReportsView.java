bb83c7ec2a244778d3e2bb66ca7ee4908ec66e0a
mustafakosker <mustafakosker@gmail.com>
2013-12-01 21:06:47 -0430

    ReportsView

---
+++ twitteranalysis/src/swe574/g2/twitteranalysis/view/ReportsView.java
@@ -0,0 +1,23 @@
+package swe574.g2.twitteranalysis.view;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+public class ReportsView extends VerticalLayout implements View{
+	
+	public void enter(ViewChangeEvent event){
+        VerticalLayout toolbar = new VerticalLayout();
+        toolbar.setWidth("100%");
+        toolbar.setSpacing(true);
+        toolbar.setMargin(true);
+        toolbar.addStyleName("toolbar");
+        addComponent(toolbar);
+
+        Label header = new Label("Reports");
+        header.addStyleName("h1");
+        toolbar.addComponent(header);
+	}
+}
